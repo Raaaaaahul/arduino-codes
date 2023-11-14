af6d1cc5ade146635d250876cc80b6fcf3cb53d0
Rahul <92531518+Raaaaaahul@users.noreply.github.com>
2023-11-14 18:09:34 +0530

    CH#4

---
+++ exp9 brightness-level.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+ledPin = 18  # Replace with the actual GPIO pin connected to the LED
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin, GPIO.OUT)
+pwm = GPIO.PWM(ledPin, 100)
+pwm.start(0)
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code", rc)
+    client.subscribe("IOTIF/Rpi/PWM")
+
+def on_message(client, userdata, msg):
+    brightness = int(msg.payload)
+    print(f"Received brightness: {brightness}")
+    pwm.ChangeDutyCycle(brightness)
+
+client = mqtt.Client()
+client.connect("broker.hivemq.com")
+client.on_connect = on_connect
+client.on_message = on_message
+
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    exit()
